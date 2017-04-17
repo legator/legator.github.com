3ecc79d9f5e485a5fb0d912b2ba6064b4fe45c80
Oleg <oleg.legator@gmail.com>
2017-04-17 19:20:51 +0300

    First step to integrate Vue.js

---
+++ blog/js/app.vue
@@ -0,0 +1,17 @@
+const Post = { template: ''}
+const Twitter = { template: '' }
+const Main = { template: '' }
+
+const routes = [
+  { path: '/post', component: Post },
+  { path: '/twitter', component: Twitter },
+  { path: '/', component: Main }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+const app = new Vue({
+  router
+}).$mount('#app')
